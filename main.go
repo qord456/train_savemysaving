3e5f662e8eba4452c15d490a41bd0b1e2c269087
robysetyaw <robysetyaw@gmail.com>
2023-06-20 17:41:21 +0700

    [FEATURE] ADD Menu Switch Case and useIfElse Looping

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
+import (
+	useifelse "challenge-git-team/useIfElse"
+	"fmt"
+)
 
 func main() {
+	var menu int
 	fmt.Println("1. Bermain Dengan Perulangan")
 	fmt.Println("2. Bermain Dengan Irisan Array")
 	fmt.Println("3. Menggunakan Switch Case")
 	fmt.Println("4. Menggunakan If Else")
+	fmt.Scanln(&menu)
+
+	switch menu {
+	case 1:
+		// Ubah ini untuk packagemu
+	case 2:
+		// Ubah ini untuk packagemu
+	case 3:
+		// Ubah ini untuk packagemu
+	case 4:
+		useifelse.Useifelse()
+	}
+
 }
--- useIfElse/useIfElse.go
+++ useIfElse/useIfElse.go
@@ -2,23 +2,26 @@ package useifelse
 
 import "fmt"
 
-func useifelse() {
+func Useifelse() {
 	var nama string
 	var peran string
 
 	fmt.Print("Masukkan nama: ")
 	fmt.Scan(&nama)
 
-	fmt.Print("Masukkan peran (superhero/monster): ")
-	fmt.Scan(&peran)
+	for peran == "" {
+		fmt.Print("Masukkan peran (superhero/monster): ")
+		fmt.Scan(&peran)
+	}
 
-	if nama == "" || peran == "" {
-		fmt.Println("Nama dan Peran Harus Di Isi")
+	if nama == "" {
+		fmt.Println("Nama Harus Di Isi")
 	} else if peran == "superhero" {
 		fmt.Println("Selamat Datang Superhero", nama+", Kalahkan Semua Monster Di Muka Bumi")
 	} else if peran == "monster" {
 		fmt.Println("Selamat Datang Monster", nama+", Hancurkan Semua Superhero Yang Ada")
 	} else {
 		fmt.Println("Selamat Datang", nama+", Pilih Peranmu Untuk Melanjutkan Game Ini")
+		Useifelse()
 	}
 }
