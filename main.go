3b24b0530b18d0cf61645eee0500b40251814031
robysetyaw <robysetyaw@gmail.com>
2023-06-20 17:04:44 +0700

    INIT CHALLENGE-GIT-TEAM

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1. Bermain Dengan Perulangan")
+	fmt.Println("2. Bermain Dengan Irisan Array")
+	fmt.Println("3. Menggunakan Switch Case")
+	fmt.Println("4. Menggunakan If Else")
+}
